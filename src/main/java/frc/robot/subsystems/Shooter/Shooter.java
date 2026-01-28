42f7bcc96d639a05efc444cb1f77f2d7a7c89b34
team7890 <team7890@gmail.com>
2026-01-28 17:07:27 -0500

    Shooter Base

--- src/main/java/frc/robot/subsystems/Shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter/Shooter.java
@@ -4,14 +4,50 @@
 
 package frc.robot.subsystems.Shooter;
 
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.MotorIDs;
 
 public class Shooter extends SubsystemBase {
+  private TalonFX objShooter = new TalonFX(MotorIDs.iShooter);
+  private StatusCode objTalonFXStatusCode;
+  private StatusSignal objStatusSignal;
+  private int iCount = 0;
+  private Runnable objShooterRunnable;
+  
   /** Creates a new Shooter. */
-  public Shooter() {}
+  public Shooter() {
+
+    TalonFXConfiguration objTalonFXConfig = new TalonFXConfiguration();
+    objTalonFXConfig.CurrentLimits.SupplyCurrentLimit = 100.0;
+    objTalonFXConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
+    objTalonFXConfig.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+    objTalonFXConfig.OpenLoopRamps.VoltageOpenLoopRampPeriod = 0.5;
+    objTalonFXStatusCode = StatusCode.StatusCodeNotInitialized;
+
+    for (int i = 1; i < 5; i++) {
+      objTalonFXStatusCode = objShooter.getConfigurator().apply(objTalonFXConfig);
+      if (objTalonFXStatusCode.isOK()) break;
+    }
+
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  }
+
+   
+  public void stopShooter(){
+    objShooter.stopMotor();
+  }
+
+  public void runShooter(double dSpeed){
+    objShooter.set(dSpeed);
   }
 }
