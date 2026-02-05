3e96259aae00a8c468ed58753ec85075aa58eaf0
team7890 <team7890@gmail.com>
2026-02-05 18:50:35 -0500

    No need for brakes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -72,10 +72,10 @@ public class RobotContainer {
             drivetrain.applyRequest(() -> idle).ignoringDisable(true)
         );
 
-        xboxDriver.a().whileTrue(drivetrain.applyRequest(() -> brake));
-        xboxDriver.b().whileTrue(drivetrain.applyRequest(() ->
-            point.withModuleDirection(new Rotation2d(-xboxDriver.getLeftY(), -xboxDriver.getLeftX()))
-        ));
+        // xboxDriver.a().whileTrue(drivetrain.applyRequest(() -> brake));
+        // xboxDriver.b().whileTrue(drivetrain.applyRequest(() ->
+        //     point.withModuleDirection(new Rotation2d(-xboxDriver.getLeftY(), -xboxDriver.getLeftX()))
+        // ));
 
         // Run SysId routines when holding back/start and X/Y.
         // Note that each routine should be run exactly once in a single log.
